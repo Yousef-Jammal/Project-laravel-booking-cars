3bb85573b3f928ef31c7a60d72e711a86fea2cf9
Yousef-Jammal <ywsfalsaydh@gmail.com>
2024-08-22 19:18:11 +0300

    Add files via upload

---
+++ database/migrations/2024_08_22_063306_create_payments_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('rental_id')->constrained('rentals')->onDelete('cascade');
+            $table->timestamp('payment_date')->useCurrent();
+            $table->decimal('amount', 10, 2);
+            $table->enum('payment_method', ['credit_card', 'debit_card', 'cash', 'bank_transfer']);
+            $table->enum('status', ['pending', 'completed', 'failed'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
