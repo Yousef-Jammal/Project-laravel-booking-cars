0f7bcc5e60dcdedb62b09d495e4e819fe27bdd82
Osama <osamaalhyari99@gmail.com>
2024-08-23 17:19:14 +0300

    connected car controller with car list

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('listing-list');
 });
+
+Route::get('/carlist', [CarController::class, 'index']);
