0999ae49dbd7fe7f3c69b2d3b6e5e2bc0948f40e
KHawara <mohamed.alkhawara@yahoo.com>
2024-08-23 19:08:32 +0300

    v2

--- resources/views/listing-list.blade.php
+++ resources/views/listing-list.blade.php
@@ -64,17 +64,255 @@
             <div class="col-xl-3 col-lg-4 col-sm-12 col-12 theiaStickySidebar">
                 <!-- Filter Form -->
                 <form action="#" autocomplete="off" class="sidebar-form">
-                    <div class="sidebar-heading">
-                        <h3>What Are You Looking For</h3>
-                    </div>
-                    <div class="product-search">
-                        <div class="form-custom">
-                            <input type="text" class="form-control" id="member_search1" placeholder="Search for a car">
-                            <span><img src="{{ asset('img/icons/search.svg') }}" alt="img"></span>
-                        </div>
-                    </div>
-                    <!-- Other filter options can be added here -->
-                </form>
+
+<div class="sidebar-heading">
+<h3>What Are You Looking For</h3>
+</div>
+<div class="product-search">
+<div class="form-custom">
+<input type="text" class="form-control" id="member_search1" placeholder>
+<span><img src="assets/img/icons/search.svg" alt="img"></span>
+</div>
+</div>
+<div class="accordion" id="accordionMain1">
+<div class="card-header-new" id="headingOne">
+<h6 class="filter-title">
+<a href="javascript:void(0);" class="w-100" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+Car Category
+<span class="float-end"><i class="fa-solid fa-chevron-down"></i></span>
+</a>
+</h6>
+</div>
+<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample1">
+<div class="card-body-chat">
+<div class="row">
+<div class="col-md-12">
+<div id="checkBoxes1">
+<div class="selectBox-cont">
+<label class="custom_check w-100">
+<input type="checkbox" name="username">
+<span class="checkmark"></span> Tesla
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="username">
+<span class="checkmark"></span> Ford
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="username">
+<span class="checkmark"></span> Mercediz Benz
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="username">
+<span class="checkmark"></span> Audi
+</label>
+
+<div class="view-content">
+<div class="viewall-One">
+<label class="custom_check w-100">
+<input type="checkbox" name="username">
+<span class="checkmark"></span> Kia
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="username">
+<span class="checkmark"></span> Honda
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="username">
+<span class="checkmark"></span> Toyota
+</label>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="accordion" id="accordionMain2">
+<div class="card-header-new" id="headingTwo">
+<h6 class="filter-title">
+<a href="javascript:void(0);" class="w-100 collapsed" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+Car Type
+<span class="float-end"><i class="fa-solid fa-chevron-down"></i></span>
+</a>
+</h6>
+</div>
+<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample2">
+<div class="card-body-chat">
+<div id="checkBoxes2">
+<div class="selectBox-cont">
+<label class="custom_check w-100">
+<input type="checkbox" name="username">
+<span class="checkmark"></span> Convertible
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="username">
+<span class="checkmark"></span> Crossover
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="username">
+<span class="checkmark"></span> Sedan
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="username">
+<span class="checkmark"></span> Wagon
+</label>
+
+<div class="view-content">
+<div class="viewall-One">
+<label class="custom_check w-100">
+<input type="checkbox" name="username">
+<span class="checkmark"></span> Pickup
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="username">
+<span class="checkmark"></span> Wagon
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="username">
+<span class="checkmark"></span> SUV
+</label>
+</div>
+</div>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="accordion" id="accordionMain3">
+<div class="card-header-new" id="headingThree">
+<h6 class="filter-title">
+<a href="javascript:void(0);" class="w-100 collapsed" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+Capacity
+<span class="float-end"><i class="fa-solid fa-chevron-down"></i></span>
+</a>
+</h6>
+</div>
+<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample3">
+<div class="card-body-chat">
+<div id="checkBoxes3">
+<div class="selectBox-cont">
+<label class="custom_check w-100">
+<input type="checkbox" name="bystatus">
+<span class="checkmark"></span> 1-4
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="bystatus">
+<span class="checkmark"></span> 1-6
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="bystatus">
+<span class="checkmark"></span> 4-6
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="bystatus">
+<span class="checkmark"></span>4-8
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="bystatus">
+<span class="checkmark"></span>8+
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="bystatus">
+<span class="checkmark"></span> 10+
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="bystatus">
+<span class="checkmark"></span> 15+
+</label>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<div class="accordion" id="accordionMain4">
+<div class="card-header-new" id="headingFour">
+<h6 class="filter-title">
+<a href="javascript:void(0);" class="w-100 collapsed" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="true" aria-controls="collapseFour">
+Price
+<span class="float-end"><i class="fa-solid fa-chevron-down"></i></span>
+</a>
+</h6>
+</div>
+<div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample4">
+<div class="card-body-chat">
+<div class="filter-range">
+<input type="text" class="input-range">
+</div>
+</div>
+</div>
+</div>
+
+<div class="accordion" id="accordionMain5">
+<div class="card-header-new" id="headingFive">
+<h6 class="filter-title">
+<a href="javascript:void(0);" class="w-100 collapsed" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="true" aria-controls="collapseFive">
+Rating
+<span class="float-end"><i class="fa-solid fa-chevron-down"></i></span>
+</a>
+</h6>
+</div>
+<div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample5">
+<div class="card-body-chat">
+<div id="checkBoxes4">
+<div class="selectBox-cont">
+<label class="custom_check w-100">
+<input type="checkbox" name="category">
+<span class="checkmark"></span>
+<i class="fas fa-star filled"></i>
+<i class="fas fa-star filled"></i>
+<i class="fas fa-star filled"></i>
+<i class="fas fa-star filled"></i>
+<i class="fas fa-star filled"></i>
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="category">
+<span class="checkmark"></span>
+<i class="fas fa-star filled"></i>
+<i class="fas fa-star filled"></i>
+<i class="fas fa-star filled"></i>
+<i class="fas fa-star filled"></i>
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="category">
+<span class="checkmark"></span>
+<i class="fas fa-star filled"></i>
+<i class="fas fa-star filled"></i>
+<i class="fas fa-star filled"></i>
+</label>
+<label class="custom_check w-100">
+<input type="checkbox" name="category">
+<span class="checkmark"></span>
+<i class="fas fa-star filled"></i>
+<i class="fas fa-star filled"></i>
+</label>
+<div class="view-content">
+<div class="viewall-Two">
+<label class="custom_check w-100">
+<input type="checkbox" name="username">
+<span class="checkmark"></span>
+<i class="fas fa-star filled"></i>
+</label>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+<button type="submit" class="d-inline-flex align-items-center justify-content-center btn w-100 btn-primary filter-btn">
+<span><i class="feather-filter me-2"></i></span>Filter results
+</button>
+<a href="#" class="reset-filter">Reset Filter</a>
+</form>
             </div>
             <div class="col-xl-9 col-lg-8 col-sm-12 col-12">
                 <div class="row">
@@ -165,6 +403,12 @@
             </div>
         </div>
     </div>
+
+    
 </section>
+
+
+
+
 
 @endsection
