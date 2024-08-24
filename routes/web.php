4dfb0af9eb75c09e7dbc25579b486ed8bb23b0c0
fajer <fajerabbadi98@gmail.com>
2024-08-24 07:08:08 +0300

    add fajer route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarDetailsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/listing-owner/{id}', [CarDetailsController::class, 'showOwnerDetails'])->name('listing.owner');
+
+Route::post('/reviews/store', [CarDetailsController::class, 'storeReview'])->name('reviews.store');
+
+Route::get('/car/{id}/reviews', [CarDetailsController::class, 'showReviews'])->name('car.reviews');
+
+Route::get('/listing-details/{id}', [CarDetailsController::class, 'show'])->name('cardetails.show');
+
+Route::get('/car/{id}/features', [CarDetailsController::class, 'showFeatures'])->name('car.features');
+
+Route::get('/listing-details', function () {
+    return view('listing-details');
+});
 
 Route::get('/', function () {
     return view('listing-list');
