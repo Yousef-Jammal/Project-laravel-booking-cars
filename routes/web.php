96a86c480f1f864da266e328681d310192f9cb30
yousef <ywsfalsaydh@gmail.com>
2024-08-24 19:48:17 +0300

    push first current branch v 1.01

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('listing-list');
+// yousef routes start
+use App\Http\Controllers\HomeController;
+
+Route::prefix('home')->group(function(){
+
+    Route::get('/', [HomeController::class, 'index']);
+
+    Route:: view('/viewCars','listing-list')->name('allCars');
+
+    Route:: view('/viewCars-f','listing-list')->name('pruduct_list_from_home_with_filter');  // this node to handle from listing-list page
+
+    Route::post('/s', [HomeController::class, 'search_rentals'] )->name('search_home'); //  I want this have more idea
+
+    Route::get('/{id}', [HomeController::class, 'go_to_pruduct_details'] )->name('pruduct_details');
+
+    Route::post('/ajax_search', [HomeController::class, 'ajax_search'])->name('ajax_search_user');
 });
+
+
+// yousef routes end
